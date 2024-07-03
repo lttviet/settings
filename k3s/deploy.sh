9419506b6d0516b16c5152cce24647f76ce3e174
Viet Le <lttviet@gmail.com>
2024-07-03 12:21:09 -0400

    cert-manager and tls for k3s.

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -6,3 +6,6 @@ kubectl apply -f homepage.yaml
 kubectl apply -f sillytavern.yaml
 kubectl apply -f navidrome.yaml
 kubectl apply -f cloudflared.yaml
+
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.1/cert-manager.yaml
+kubectl apply -f cert.yaml
