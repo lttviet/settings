6bc58f5de73415d4f79cec042973408421d39801
Viet Le <lttviet@gmail.com>
2024-07-03 10:19:38 -0400

    Add navidrome k3s.

--- k3s/deploy.sh
+++ k3s/deploy.sh
@@ -4,4 +4,5 @@ kubectl apply -f namespace.yaml
 sops -d secrets.yaml | kubectl apply -f -
 kubectl apply -f homepage.yaml
 kubectl apply -f sillytavern.yaml
+kubectl apply -f navidrome.yaml
 kubectl apply -f cloudflared.yaml
