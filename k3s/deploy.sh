a6f70f56c9fca0cea6573b3ce00df51bce99b5de
Viet Le <lttviet@gmail.com>
2024-07-03 10:06:30 -0400

    Add k3s templates.

---
+++ k3s/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+kubectl apply -f namespace.yaml
+sops -d secrets.yaml | kubectl apply -f -
+kubectl apply -f homepage.yaml
+kubectl apply -f sillytavern.yaml
+kubectl apply -f cloudflared.yaml
