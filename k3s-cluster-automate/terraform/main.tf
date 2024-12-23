73d74877701e03f6431804c1584a9c9f17820c0e
Viet Le <lttviet@gmail.com>
2024-12-23 02:50:42 -0500

    Provision NAS and K3s VMs.

--- k3s-cluster-automate/terraform/main.tf
+++ k3s-cluster-automate/terraform/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "0.69.0"
+      version = "~> 0.69.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "2.5.2"
+      version = "~> 2.5.2"
     }
     sops = {
       source  = "carlpett/sops"
-      version = "1.1.1"
+      version = "~> 1.1.1"
     }
   }
 }
@@ -21,83 +21,155 @@ data "sops_file" "secrets" {
   source_file = "${path.module}/secrets-tf.yaml"
 }
 
+locals {
+  pve_endpoint    = data.sops_file.secrets.data["pve_endpoint"]
+  pve_username    = data.sops_file.secrets.data["pve_username"]
+  pve_password    = data.sops_file.secrets.data["pve_password"]
+  samba_username  = data.sops_file.secrets.data["samba_username"]
+  samba_password  = data.sops_file.secrets.data["samba_password"]
+  k3s_token       = data.sops_file.secrets.data["k3s_token"]
+  ssh_public_keys = yamldecode(data.sops_file.secrets.raw).ssh_public_keys
+}
+
 provider "proxmox" {
-  endpoint = data.sops_file.secrets.data["pve_endpoint"]
-  username = data.sops_file.secrets.data["pve_username"]
-  password = data.sops_file.secrets.data["pve_password"]
+  endpoint = local.pve_endpoint
+  username = local.pve_username
+  password = local.pve_password
   insecure = true
 }
 
-resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_images" {
-  for_each = var.k3s_nodes
+# Download image to specified PVE nodes
+resource "proxmox_virtual_environment_download_file" "cloud_images" {
+  for_each = toset(var.pve_nodes)
 
   content_type = "iso"
   datastore_id = "local"
-  node_name    = each.value.pve_node
-  url          = "https://cloud-images.ubuntu.com/noble/20241210/noble-server-cloudimg-amd64.img"
+  node_name    = each.value
+  url          = var.image_url
 }
 
-resource "proxmox_virtual_environment_vm" "ubuntu_vms" {
+# Create snippets for NAS cloud config
+resource "proxmox_virtual_environment_file" "nas_user_data" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.nas_pve_node
+
+  source_raw {
+    data = templatefile("${path.root}/cloud-init/nas-user-data.yaml.tftpl", {
+      username        = var.username
+      ssh_public_keys = local.ssh_public_keys
+      samba_username  = local.samba_username
+      samba_password  = local.samba_password
+    })
+
+    file_name = "${var.nas_hostname}-user-data.yaml"
+  }
+}
+
+resource "proxmox_virtual_environment_file" "nas_meta_data" {
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = var.nas_pve_node
+
+  source_raw {
+    data = templatefile("${path.root}/cloud-init/meta-data.yaml.tftpl", {
+      hostname = var.nas_hostname
+    })
+
+    file_name = "${var.nas_hostname}-meta-data.yaml"
+  }
+
+}
+
+# Provision NAS VM
+module "nas_server" {
+  source = "./modules/ubuntu-vm"
+
+  hostname     = var.nas_hostname
+  username     = var.username
+  pve_node     = var.nas_pve_node
+  ipv4_address = var.nas_ipv4_address
+  ipv4_gateway = var.nas_ipv4_gateway
+
+  startup_order = "1"
+
+  cpu_cores = 4
+  memory    = 4096
+
+  disk_datastore_id = "datapool"
+  disk_size         = 500
+
+  cloud_image_file_id = proxmox_virtual_environment_download_file.cloud_images[var.nas_pve_node].id
+  user_data_file_id   = proxmox_virtual_environment_file.nas_user_data.id
+  meta_data_file_id   = proxmox_virtual_environment_file.nas_meta_data.id
+}
+
+# Create snippets for K3S VM cloud configs
+resource "proxmox_virtual_environment_file" "k3s_user_data_list" {
   for_each = var.k3s_nodes
 
-  name      = each.key
-  node_name = each.value.pve_node
-  vm_id     = each.value.vm_id
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = each.value.pve_node
 
-  agent {
-    enabled = true
-  }
+  source_raw {
+    data = templatefile("${path.root}/cloud-init/base-user-data.yaml.tftpl", {
+      username        = var.username
+      ssh_public_keys = local.ssh_public_keys
+      nas_server = var.nas_ipv4_address
+    })
 
-  startup {
-    order    = "3"
-    up_delay = "60"
-  }
-
-  initialization {
-    ip_config {
-      ipv4 {
-        address = "dhcp"
-      }
-    }
-
-    datastore_id      = "local-zfs"
-    user_data_file_id = proxmox_virtual_environment_file.user_data_cloud_configs[each.key].id
-    meta_data_file_id = proxmox_virtual_environment_file.meta_data_cloud_configs[each.key].id
-  }
-
-  disk {
-    datastore_id = "local-zfs"
-    file_id      = proxmox_virtual_environment_download_file.ubuntu_cloud_images[each.key].id
-    interface    = "scsi0"
-    discard      = "on"
-    size         = 10
-    ssd          = true
-  }
-
-  memory {
-    dedicated = 8192
-  }
-
-  cpu {
-    type  = "x86-64-v2-AES"
-    cores = each.value.cores
-  }
-
-  network_device {
-    bridge = "vmbr0"
-  }
-
-  serial_device {
-    device = "socket"
+    file_name = "${each.key}-user-data.yaml"
   }
 }
 
+resource "proxmox_virtual_environment_file" "k3s_meta_data_list" {
+  for_each = var.k3s_nodes
+
+  content_type = "snippets"
+  datastore_id = "local"
+  node_name    = each.value.pve_node
+
+  source_raw {
+    data = templatefile("${path.root}/cloud-init/meta-data.yaml.tftpl", {
+      hostname = each.key
+    })
+
+    file_name = "${each.key}-meta-data.yaml"
+  }
+}
+
+# Provision K3S VMs
+module "k3s_vms" {
+  for_each = var.k3s_nodes
+
+  source = "./modules/ubuntu-vm"
+
+  hostname     = each.key
+  username     = var.username
+  pve_node     = each.value.pve_node
+  ipv4_address = "dhcp"
+
+  startup_order = "3"
+
+  cpu_cores = each.value.cpu_cores
+  memory    = 8192
+
+  disk_datastore_id = "local-zfs"
+  disk_size         = 10
+
+  cloud_image_file_id = proxmox_virtual_environment_download_file.cloud_images[each.value.pve_node].id
+  user_data_file_id   = proxmox_virtual_environment_file.k3s_user_data_list[each.key].id
+  meta_data_file_id   = proxmox_virtual_environment_file.k3s_meta_data_list[each.key].id
+}
+
+# Generate ansible inventory.yaml
 locals {
-  inventory_content = templatefile("${path.module}/../ansible/inventory.yaml.tftpl", {
+  inventory_content = templatefile("${path.root}/ansible/inventory.yaml.tftpl", {
     nodes = {
-      for k, v in proxmox_virtual_environment_vm.ubuntu_vms :
+      for k, v in module.k3s_vms:
       k => {
-        ip   = one(flatten(v.ipv4_addresses[1]))
+        ip   = v.ipv4_address
         role = var.k3s_nodes[k].role
       }
     }
@@ -107,5 +179,13 @@ locals {
 
 resource "local_file" "inventory" {
   content  = local.inventory_content
-  filename = "${path.module}/../ansible/inventory.yaml"
+  filename = "${path.module}/ansible/inventory.yaml"
+}
+
+output "nas_ip" {
+  value = module.nas_server.ipv4_address
+}
+
+output "k3s_ips" {
+  value = [for vm in module.k3s_vms : vm.ipv4_address]
 }
