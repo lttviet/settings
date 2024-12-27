8f4cfd4db6a211dc626f02a438ec3f5cd504307c
Viet Le <lttviet@gmail.com>
2024-12-26 23:19:39 -0500

    From now on, use enc for encrypted secrets

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "sops" {}
 
 data "sops_file" "secrets" {
-  source_file = "${path.module}/secrets-tf.yaml"
+  source_file = "${path.module}/secrets-tf.enc.yaml"
 }
 
 locals {
